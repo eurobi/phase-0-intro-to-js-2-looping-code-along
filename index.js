c9505d0d1c66a4dcbf7c89f784501ad95fc9567c
Jeremy Barbara <jeremy.w.barbara@gmail.com>
2022-08-10 14:24:05 -0400

    commit tree 7a74baceabc7de438d14566ca3c5a99eca2ab68c

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function countDown(start){
+    for(let i = start; i >= 0; i--){
+        console.log(i)
+    }
+
+}
+
+function writeCards(names, occasion){
+    const messages = []
+    for(let name of names){
+        messages.push(`Thank you, ${name}, for the wonderful ${occasion} gift!`)
+    }
+    return messages
+}
